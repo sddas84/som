91ad70aa132e60236b657a2e6fab90dba63ef3c6
sddas84 <somadebdas@gmail.com>
2018-09-30 11:43:25 +0530

    sdghj

---
+++ 30th sep/untitled0.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 11:18:18 2018
+
+@author: Niladri
+"""
+
---
+++ knn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 10:29:53 2018
+
+@author: Niladri
+"""
+import os
+os.getcwd()
+os.chdir("C:\\Users\\Niladri\\Desktop\\Grey Atom\\Data")
+os.getcwd()
+import pandas as pd
+df = pd.read_csv("dc_airbnb.csv")
+df.shape()
